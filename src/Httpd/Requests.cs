24bb8acf2505e556b139b3e175cd466222277306
Bergy <53085640+acpb-dev@users.noreply.github.com>
2022-03-18 15:16:40 -0400

    Added Support for images
    Works with full website

--- src/Httpd/Requests.cs
+++ src/Httpd/Requests.cs
@@ -1,84 +1,74 @@
-﻿using Httpd;
+﻿using System.Net.Mime;
+using System.Text;
+using Httpd;
 
 public class Requests
 {
-    private string _typeOf = "0";
     
-    private ReadHTML _readHtml = new ReadHTML();
-    public string ManageRequest(string request)
+    private readonly ReadHTML _readHtml = new ReadHTML();
+    public byte[] ManageRequest(string request)
     {
-        StringReader strReader = new StringReader(request);
-        string aLine = strReader.ReadLine();
-        //Console.WriteLine(aLine);
-        string[] splittedStream = aLine.Split();
-        if (splittedStream[0].Equals("GET") && splittedStream[1].Equals("/"))
+        var strReader = new StringReader(request);
+        var aLine = strReader.ReadLine();
+        var splitStream = aLine.Split();
+        if (splitStream[0].Equals("GET") && splitStream[1].Equals("/"))
         {
-            string[] test = _readHtml.ReadHtmlFromRoute();
-            return Html("default", splittedStream[1]) + _typeOf;
+            var test = _readHtml.ReadHtmlFromRoute();
+            return Html("default", splitStream[1]);
         }
         else
         {
-            return Html("!default", splittedStream[1]) + _typeOf;
+            return Html("!default", splitStream[1]);
         }
-        return "";
     }
 
-    private string Html(string type, string path)
+    private byte[] Html(string type, string path)
     {
-        string text = "";
-        if (type.Equals("default"))
-        {
-            text = SearchIndex();
-        }
-        else
-        {
-            //Console.WriteLine(path);
-            text = ReadSpecifiedFiles(path);
-        }
-        
+        var text = type.Equals("default") ? SearchIndex() : ReadSpecifiedFiles(path);
         var response = @"HTTP/1.1 200 OK\r\n";
         response += "Content-Length: 44";
         response += "Content-Type: text/html";
         response += "Connection: close\r\n";
         response += "\r\n";
-        response += text;
-        return response;
+        byte[] temp = Encoding.UTF8.GetBytes(response);
+        byte[] z = new byte[temp.Length + text.Length];
+        temp.CopyTo(z, 0);
+        text.CopyTo(z, temp.Length);
+        return z;
     }
-    
 
-    private string ReadSpecifiedFiles(string path)
+    private byte[] ReadSpecifiedFiles(string path)
     {
         string[] temp = path.Split(".");
-        string extension = temp[temp.Length - 1];
+        string extension = temp[^1];
         if (extension.Equals("jpg"))
         {
-            _typeOf = "1";
-            Console.WriteLine("SADGSADGASDGASDGSADGASDGASDGASDG");
             path.TrimStart('/');
+            return File.ReadAllBytes(path.TrimStart('/'));
         }
         else if (path[0].Equals('/'))
         {
-            return File.ReadAllText(path.TrimStart('/'));
+            return Encoding.UTF8.GetBytes(File.ReadAllText(path.TrimStart('/')));
         }
-        return File.ReadAllText(path);
+        return Encoding.UTF8.GetBytes(File.ReadAllText(path));
     }
 
-    private string SearchIndex()
+    private byte[] SearchIndex()
     {
         string[] test = _readHtml.ReadHtmlFromRoute();
-        bool valide = false;
+        bool valid = false;
         foreach (var variable in test)
         {
             var result = variable.Substring(variable.Length - 10);
             if (result.Equals("index.html"))
             {
-                valide = true;
+                valid = true;
             }
         }
-        if (valide)
+        if (valid)
         {
-            return File.ReadAllText("index.html");
+            return Encoding.UTF8.GetBytes(File.ReadAllText("index.html"));
         }
-        return "<!DOCTYPE html> <html lang=\"en\"> <head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>404</title> </head> <body> <div class=\"div\">404 Page not found</div> </body> </html>";
+        return Encoding.UTF8.GetBytes("<!DOCTYPE html> <html lang=\"en\"> <head> <meta charset=\"UTF-8\"> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"> <title>404</title> </head> <body> <div class=\"div\">404 Page not found</div> </body> </html>");
     }
 }
--- src/Httpd/Server.cs
+++ src/Httpd/Server.cs
@@ -9,23 +9,22 @@ namespace Httpd;
 
 public class Server
 {
-    private TcpListener _listenner;
-    public int Port { get; set; }
-    public Requests requests = new Requests();
-    public ReadHTML ReadHtml = new ReadHTML();
+    private readonly TcpListener _listener;
+    private int Port { get; set; }
+    private readonly Requests _requests = new Requests();
     public Server(int port)
     {
         Port = port;
-        _listenner = TcpListener.Create(Port);
+        _listener = TcpListener.Create(Port);
     }
 
     public async Task Start()
     {
-        _listenner.Start();
+        _listener.Start();
         Console.WriteLine($"Server has started on port {Port}.");
         while (true)
         {
-            var client = await _listenner.AcceptTcpClientAsync();
+            var client = await _listener.AcceptTcpClientAsync();
             HandleRequest(client);
         }
     }
@@ -34,24 +33,11 @@ public class Server
     {
         Thread.Sleep(50);
         var stream = client.GetStream();
-        
-        // Console.WriteLine($"--- Request Count #{_requestCount} ---");
-        // Console.WriteLine($"--- End of request ---");
-        
         var socket = stream.Socket;
         var buffer = new byte[socket.Available];
         stream.Read(buffer, 0, buffer.Length);
-        
         var data = Encoding.UTF8.GetString(buffer);
-        
-        var plainTextBytes = Encoding.UTF8.GetBytes(data);
-        //Console.WriteLine(Convert.ToBase64String(plainTextBytes));
-        var temper = requests.ManageRequest(data);
-        // if (temper[temper.Length-1].Equals("1"))
-        // {
-        //     responsesByte
-        // }
-        var responsesByte = Encoding.UTF8.GetBytes(temper);
+        var responsesByte = _requests.ManageRequest(data);
         
         socket.Send(responsesByte);
         socket.Close();
