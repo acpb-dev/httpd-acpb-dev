0a279c11080afd512809ef4135448093d2b7389c
Bergy <53085640+acpb-dev@users.noreply.github.com>
2022-03-28 15:56:23 -0400

    Revert "Revert "Added Test Classes & acutal tests""
    This reverts commit a8d12244546ef92d57bd7c074572622d7a6bbbbd.

---
+++ test/Httpd.Tests/FileReaderTests.cs
@@ -0,0 +1,51 @@
+namespace Httpd.Tests;
+
+public class FileReaderTests
+{
+    private static Dictionary<string, string> testDictionary = new()
+    {
+        { "apng", "image/apng" },
+        { "avif", "image/avif" },
+        { "gif", "image/gif" },
+        { "jpg", "image/jpeg" },
+        { "jpeg", "image/jpeg" },
+        { "jfif", "image/jpeg" },
+        { "pjpeg", "image/jpeg" },
+        { "pjp", "image/jpeg" },
+        { "png", "image/png" },
+        { "svg", "image/svg+xml" },
+        { "bmp", "image/bmp" },
+        { "ico", "image/vnd.microsoft.icon" },
+        { "tif", "image/tiff" },
+        { "tiff", "image/tiff" },
+        { "webp", "image/webp" },
+        { "html", "text/html" },
+        { "htm", "text/html" },
+        { "js", "text/javascript" },
+        { "css", "text/css" },
+        { "mjs", "text/javascript" },
+        { "txt", "text/plain" },
+        { "php", "application/x-httpd-php" }
+    };
+    
+    [Theory]
+    [InlineData("html", "text/html")]
+    [InlineData("jpeg", "image/jpeg")]
+    [InlineData("php", "application/x-httpd-php")]
+    [InlineData("nothing", "N/A")]
+    [InlineData("", "N/A")]
+    public void CheckExtensionTest( string valSearched , string expected)
+    {
+        var result = FileReader.CheckExtension(testDictionary, valSearched);
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData("Gzip.cs", "404", "text/html")]
+    public void ReadSpecifiedFilesTest(string path , string expected1, string expected2)
+    {
+        var result = FileReader.ReadSpecifiedFiles(path);
+        Assert.Equal(expected1, result.Item2);
+        Assert.Equal(expected2, result.Item3);
+    }
+}
