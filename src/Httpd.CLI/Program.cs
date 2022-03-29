d098c2028bcda5ec761cd841e6bce396e3c74d36
Will Bergy <53085640+acpb-dev@users.noreply.github.com>
2022-03-29 10:09:01 -0400

    Revert "Feedback"

--- src/Httpd.CLI/Program.cs
+++ src/Httpd.CLI/Program.cs
@@ -1,14 +1,1 @@
-﻿using Httpd;
-using System.Configuration;
-
-
-FileReader.ReadAppConfig();
-Console.WriteLine("My Httpd server!");
-var port = ConfigurationManager.AppSettings["port"];
-
-if (port.Equals(""))
-{
-    port = "3000";
-}
-var server = new Server(int.Parse(port));
-await server.Start();
+﻿Console.WriteLine("My Httpd server!");
